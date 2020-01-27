be52f5d29f1188190584820014443286ec137f82
mgritsuk <oneloveonemotiv@gmail.com>
2020-01-27 17:41:22 +0300

    commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -14,6 +14,7 @@ public class MainTest extends WasHomePage {
                 .logOut()
                 .logIn()
                 .logOut()
+                .logIn()
                 .logIn();
     }
 }
